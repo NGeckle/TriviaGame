e171938525660a2499682d49207808df0bde2d6f
Nathan Geckle <nathan_geckle@yahoo.com>
2018-03-03 16:27:45 -0500

    Added gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,14 @@ $(document).ready(function() {
     var answers = [["20", "18", "16", "14"], ["230", "244", "250", "225"], ["Slow Blink", "Hissing", "Purring", "Kneading"], ["Just to make noise.", "To communicate with other cats.", "To communicate with humans.", "To warn you of an attack."], ["Flock", "Squad", "Crowd", "Kindle"], ["Catteries", "Cat Builders", "Catmen", "Catographers"], ["For pets.", "For protection.", "To get rid of rodents.", "For illegal fighting."], ["1984", "1927", "1901", "1894"], ["Allogrooming", "Pinagrooming", "Fidogrooming", "There is no special term."], ["Lavender", "Maple", "Citrus", "Cherry"], ["Spraying on you.", "Rubbing their head and body all over you.", "Licking you.", "Scratching you."], ["10", "20", "16", "24"], ["46lbs", "48lbs", "39lbs", "44lbs"]]
     var correctAnswers = ["B. 18", "B. 244", "B. Hissing", "C. To communicate with humans.", "D. Kindle", "A. Catteries", "C. To get rid of rodents.", "D. 1894", "A. Allogrooming", "C. Citrus", "B. Rubbing their head and body all over you.", "C. 16", "A. 46lbs"];
     var correctCatPuns = ["Fancatstic!", "Looking Good, Feline Good!", "That was clawsome!", "Fancatstic!", "That was clawsome!", "Fancatstic!", "Looking Good, Feline Good!", "Purrfect!", "Fancatstic!", "That was pawsome!", "I hope you aren't afraid of these cat puns. That would make you a scaredy cat!", "Don't let the cat out of the bag! You're almost there!", "Purrfect!"];
-    var incorrectCatPuns = ["You've gotta be kitten me!", "Stay pawsitive!", "Are you fur real?", "Stay pawsitive!", "You've gotta be kitten me!", "You can do better than that rigt meow!", "Are you fur real?", "A cat could do better than that!", "Stay pawsitive!", "It's meow or never!", "I hope you aren't afraid of these cat puns. That would make you a scaredy cat!", "Don't let the cat out of the bag! You're almost there!", "You've gotta be kitten me!"];
+    var incorrectCatPuns = ["You've gotta be kitten me!", "Stay pawsitive!", "Are you fur real?", "Stay pawsitive!", "You've gotta be kitten me!", "You can do better than that right meow!", "Are you fur real?", "A cat could do better than that!", "Stay pawsitive!", "It's meow or never!", "I hope you aren't afraid of these cat puns. That would make you a scaredy cat!", "Don't let the cat out of the bag! You're almost there!", "You've gotta be kitten me!"];
+    var catGifs = ["assets/images/hellNo.jpg", "assets/images/cuteCat.gif", "assets/images/inbreadCat.gif", "assets/images/realization.gif", "assets/images/lollipopCat.gif", "assets/images/jumpCat.gif", "assets/images/catBanana.gif", "assets/images/catTyping.gif", "assets/images/guiltyCat.jpg", "assets/images/boopCat.gif", "assets/images/fatCat.gif", "assets/images/broccoliCat.gif", "assets/images/wellShit.gif"];
 
     $("#start").on("click", function() {
         createHTML();
         timerFunc();
         $("#rules").text("");
+        $("#button").text("");
     })
 
     $("body").on("click", ".answer", function() {
@@ -32,6 +34,7 @@ $(document).ready(function() {
     var countdown;
     var gameHTML;
     var selectedAnswer;
+    
     
 
     function timerFunc() {
@@ -72,6 +75,14 @@ $(document).ready(function() {
     function createHTML() {
         gameHTML = "<h2> Time Remaining: <span id='timer'>45</span></p><h2 id='question'>" + questions[questionCount] + "</p><h3 class='first-answer answer'>A. " + answers[questionCount][0] + "</h3><h3 class='second-answer answer'>B. " + answers[questionCount][1] + "</h3><h3 class='third-answer answer'>C. " + answers[questionCount][2] + "</h3><h3 class='fourth-answer answer'>D. " + answers[questionCount][3] + "</h3>";
         $(".main").html(gameHTML);
+
+        
+            var imageElement = document.createElement("img");
+            imageElement.setAttribute("src", catGifs[questionCount]);
+            $("#gifs").text("");
+            $("#gifs").append(imageElement);
+            
+        
     }
 
     function wait() {
@@ -90,8 +101,5 @@ $(document).ready(function() {
         gameHTML = "<h2>Time Remaining: <span id='timer'>" + timer + "</span></p><h2>We're finished! Here&#39;s your score!</h2>" + "<h3>Correct answers: " + correct + "</h3><h3>Incorrect answers: " + incorrect + "</h3>";
         $(".main").html(gameHTML);
     }
-
-
-
     
 })
